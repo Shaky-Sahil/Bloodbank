0004e9f1eef011e281909f964734c1a08362a6c1
Abhinav-2255 <85694005+Abhinav-2255@users.noreply.github.com>
2023-05-16 14:42:24 +0000

    styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import { Box, Button, IconButton, Toolbar, Typography } from '@mui/material';
 import { Link } from 'react-router-dom'
 import BloodtypeIcon from '@mui/icons-material/Bloodtype';
-import { Person, PersonAdd } from '@mui/icons-material';
 import './Nav.css'
 export default function Nav() {
   return (
@@ -22,8 +21,8 @@ export default function Nav() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Dashboard
           </Typography>
-          <Link to={'/Login'} className='Log'><Button color='error' variant="filled" sx={{textDecoration:'none'}} startIcon={<Person />}>Login</Button></Link>
-          <Link to={'/SignUp'} className='Sign'><Button color='error' variant='filled' startIcon={<PersonAdd />}>SignUp</Button></Link>
+          <Link to={'/Login'} className='Log'><Button color='error' variant="filled" sx={{textDecoration:'none'}}>Login</Button></Link>
+          <Link to={'/SignUp'} className='Sign'><Button color='error' variant='filled'>SignUp</Button></Link>
          
         </Toolbar>
       </AppBar>
