f76b0c9453cf53bb8a2cf212b1389a566270b765
ParvathyR15 <132562967+ParvathyR15@users.noreply.github.com>
2023-05-17 21:38:10 +0530

    RequestDB added

---
+++ src/model/RequestDB.jsx
@@ -0,0 +1,19 @@
+//Server Database Connection
+const mongoose = require("mongoose");
+//Connecting Database
+mongoose.connect("mongodb+srv://sahil:sahil@cluster0.n5jhj4v.mongodb.net/?retryWrites=true&w=majority")
+//Schema Creation
+const Schema = mongoose.Schema;
+var requestSchema = new  Schema({
+    requestName:String,
+    requestAge:Number,
+    requestEmail:String,
+    requestPhone:Number,
+    requestBlood:String
+    requestAilment:String,
+    requestUnit:Number
+});
+//4.Set up collections
+mongoose.model("request",userSchema)
+//5.Exporting
+module.exports=RequestInfo;
